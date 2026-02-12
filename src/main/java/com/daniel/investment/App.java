9182777cd37657894cc124583b80c8ddbc57586b
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2026-02-12 20:00:08 -0300

    first

---
+++ src/main/java/com/daniel/investment/App.java
@@ -0,0 +1,27 @@
+package com.daniel.investment;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        VBox root = new VBox(12);
+        root.setStyle("-fx-padding: 16;");
+        root.getChildren().addAll(
+                new Label("Investment Tracker"),
+                new Label("Local-first (SQLite) - JavaFX")
+        );
+
+        stage.setTitle("Investment Tracker");
+        stage.setScene(new Scene(root, 520, 200));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
